cc62161d6798c60490a175bc854988a4e8ee40f2
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2022-01-09 20:53:24 -0300

    Inicio da aplicação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
-    return (
-        <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-    );
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const [value, setValue] = useState('');
+
+  const erase = () => {
+    setValue('');
+  };
+
+  const submit = () => {
+    console.log(value);
+    erase();
+  };
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+    } else if (event.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
+  return (
+    <section id="app" className="container">
+      <header>
+        <h1 className="title">Todo</h1>
+      </header>
+      <section className="main">
+        <input
+          className="new-todo"
+          placeholder="what need to be done?"
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+      </section>
+    </section>
+  );
 }
 
 export default App;
